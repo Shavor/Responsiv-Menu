b6352e15ee5222f7359ba76557e333eda1c4f7db
shavor <shavor80@yandex.ru>
2023-12-30 15:27:32 +0300

    smooth scroll

---
+++ script.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('.nav-list li a');
+
+for(link of links) {
+  link.addEventListener('click', smoothScrol)
+}
+
+function smoothScrol(e){
+  e.preventDefault();
+  
+  let href = this.getAttribute('href')
+  document.querySelector(href).scrollIntoView({
+    behavior: 'smooth',
+  })
+}
