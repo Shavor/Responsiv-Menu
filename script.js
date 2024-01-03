74cb4f9abcf36efae9aeeb68e82cb0016f86a5fe
shavor <shavor80@yandex.ru>
2024-01-03 10:23:44 +0300

    create responsiv menu

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const links = document.querySelectorAll('.nav-list li a');
+const links = document.querySelectorAll('.nav-list li a'),
+      menuWrapper = document.querySelector('.nav-wrapper'),
+      menu = document.querySelector('.nav-list'),
+      humburger = document.querySelector('.humburger'),
+      close = document.querySelector('.close');
 
-for(link of links) {
+for (link of links) {
   link.addEventListener('click', smoothScrol)
 }
 
@@ -11,4 +15,25 @@ function smoothScrol(e){
   document.querySelector(href).scrollIntoView({
     behavior: 'smooth',
   })
+  hideMenu()
 }
+
+// RESPONSIVE MOBILE MENU 
+ 
+const showMenu = () => {
+  humburger.style.display = 'none';
+  close.style.transform = 'translateY(0)';
+  menuWrapper.style.transform = 'translateX(0)';
+  menu.style.transform = 'translateX(0)';
+}
+
+const hideMenu = () => {
+  humburger.style.display = 'block';
+  close.style.transform = 'translateY(-20rem)';
+  menuWrapper.style.transform = 'translateX(-200%)';
+  menu.style.transform = 'translateX(200%)';
+}
+
+humburger.addEventListener('click', showMenu);
+close.addEventListener('click', hideMenu);
+menuWrapper.addEventListener('click', hideMenu);
